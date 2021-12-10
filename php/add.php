3e3c473f9be2152a6f705d4c82fd168a125134b8
VanessaAdjei <vanessa.adjei@ashesi.edu.gh>
2021-12-10 04:12:30 +0000

    Changed a spelling

--- php/add.php
+++ php/add.php
@@ -36,7 +36,7 @@
                     <a class="nav-link" href="Traveldoc.php">Travel Documents</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="downloads.php">Download</a>
+                    <a class="nav-link" href="downloads.php">Downloads</a>
                 </li>
                  <li class="nav-item">
                     <a class="nav-link" href="https://www.google.com">FAQ</a>
